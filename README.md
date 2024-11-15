1405a57772c7748667da76a0e5e7be318c703dae
Nicolo <marianinicolo@itis-molinari.eu>
2024-11-15 12:30:57 +0100

    Readme e cache

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # 5B_Fassina_Pizzonia_Mariani_incidenti
+Classi:
+    Incidente
+        --Attributi--
+        indirizzo (str)
+        targhe (list)(str)
+        num_morti (int)
+        num_feriti (int)
+        data (date(ISOString))
+
+        --Metodi--
+        constructor(indirizzzo,targhe,num_morti,num_feriti,data)
+
+Componenti
+    Form
+        --Private--
+        binding_element
+        input_element
+
+        --Pubbliche--
+        bind_element(new_element)
+        config_input_element(new_element)
+        render()
+
+    Table
+        --Private--
+        binding_element
+        header_element
+
+        --Pubbliche--
+        bind_element(new_element)
+        config_header(new_element)
+        render()
+
+Functions
+
---
+++ Scripts/cache.js
@@ -0,0 +1,45 @@
+const upload = () => {
+    return new Promise(()=>{
+      fetch("conf.json").then(r => r.json()).then(confData => {
+        try{
+          fetch(confData.url + "set", {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json',
+              'key': confData.token
+            },
+            body: JSON.stringify({
+              key: confData.key,
+              value: data
+            })
+          }).then(r => r.json())
+            .then(r => {console.log(r);})
+        }
+        catch(error){
+          reject(error)
+        }
+      })    
+    })
+}
+  
+  const download = () => {
+    return new Promise((resolve,reject)=>{
+      fetch("conf.json").then(r => r.json()).then(confData => {
+        try{
+          fetch(confData.url + "get", {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json',
+              'key': confData.token
+            },
+            body: JSON.stringify({
+              key: confData.key
+            })
+          }).then(r => r.json())
+          .then(data => {resolve(data);})
+        }catch(error) {
+          reject(error)
+        }
+      })
+    })   
+  }
