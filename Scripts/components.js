77786818d057a13a9dd782eac299a4955f66a435
Nicolo Mariani <marianinicolo@itis-molinari.eu>
2024-11-17 16:15:58 +0100

    Unione components su stesso file

--- Scripts/components.js
+++ Scripts/components.js
@@ -14,6 +14,20 @@ const createObject = () => {
             document.getElementById("div_form").innerHTML = line;
         }
     }
-       
-
 } 
+
+const createTable = () => {
+    let binding_element;
+    let header_element;
+    return{
+        bind_element: (new_element) => {
+            binding_element = new_element;
+        },
+        config_header: (new_element) => {
+            header_element = new_element;
+        },
+        render: () => {
+
+        }
+    }
+}
