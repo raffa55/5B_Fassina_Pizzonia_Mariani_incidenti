a9c61e9ef47be9f6017b7a50a4eba30fef0ce4e6
raffaele fassina <fassinaraffaele@itis-molinari.eu>
2024-11-17 16:33:58 +0100

    creazione classe incidente

---
+++ Scripts/classes.js
@@ -0,0 +1,10 @@
+class Incidente {
+    constructor(indirizzo, targhe_coinvolte, data, ora, numero_feriti, numero_morti) {
+        this.indirizzo = indirizzo;
+        this.targhe_coinvolte = targhe_coinvolte;
+        this.data = data;
+        this.ora = ora;
+        this.numero_feriti = numero_feriti;
+        this.numero_morti = numero_morti
+    }
+}
